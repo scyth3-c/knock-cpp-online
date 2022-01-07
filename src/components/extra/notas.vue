81d5233cf4e0ee97ae27badcb063c1d9bcfb7f0f
scyth3-c <kevintor2246@gmail.com>
2022-01-07 16:18:01 -0500

    vuex added

--- src/components/extra/notas.vue
+++ src/components/extra/notas.vue
@@ -1,149 +1,181 @@
 <template>
+  <b-container>
+    <b-card v-if="init == false">
+      <b-badge variant="danger">Hola! para inicia dime tu nombre</b-badge>
+      <b-input class="mt-2" v-model="nombre"></b-input>
+      <b-button class="btn-sm mt-2" variant="primary" @click="saveme"
+        >guardame</b-button
+      >
+    </b-card>
 
-
-    <b-container>
-            
-  <b-card v-if="init==false">
-    
-    <b-badge variant="danger">Hola! para inicia dime tu nombre</b-badge>
-    <b-input class="mt-2" v-model="nombre"></b-input>
-     <b-button class="btn-sm mt-2" variant="primary"  @click="saveme">guardame</b-button>
-
-  </b-card> 
-
-   <b-card v-if="init==true">
-   
-   <b-badge variant="dark">aqui apareceran tus notas</b-badge>
-    <b-card  class="rounded" style="min-height: 200px; max-height: 200px; overflow:auto; border: none;">
-      <b-table hover :items="filterData" :fields="field" >
-          <template #cell(nombre)="data"> 
-             <button class="btn btn-large btn-primary" @click="showNote(data.item._id)"  style="min-width: 150px;" >{{data.value}}</button>
-            <b-button class="btn-sm" variant="white" @click="deleteNote(data.item._id)"><b-icon variant="red" icon="trash"></b-icon></b-button>
+    <b-card v-if="init == true">
+      <b-badge variant="dark">aqui apareceran tus notas</b-badge>
+      <b-card
+        class="rounded"
+        style="min-height: 200px; max-height: 200px; overflow:auto; border: none;"
+      >
+        <b-table hover :items="filterData" :fields="field">
+          <template #cell(nombre)="data">
+            <button
+              class="btn btn-large btn-primary"
+              @click="showNote(data.item._id)"
+              style="min-width: 150px;"
+            >
+              {{ data.value }}
+            </button>
+            <b-button
+              class="btn-sm"
+              variant="white"
+              @click="deleteNote(data.item._id)"
+              ><b-icon variant="red" icon="trash"></b-icon
+            ></b-button>
           </template>
         </b-table>
-       </b-card>
-        
-        <b-button class="btn-sm ml-2" @click="$bvModal.hide('notas-modal')" variant="secondary">cerrar</b-button>
-        <b-button variant="success" class="ml-2 btn-sm"  v-b-modal.add-note>agregar</b-button>
-          <b-modal id="add-note" title="extra" @ok="sendNote">
-            <b-badge variant="dark">nombre</b-badge>
-            <b-input class="my-2" v-model="nuevaNota.nombre"></b-input>
-            <b-badge variant="dark">contenido</b-badge>
-            <b-textarea class="my-2" v-model="nuevaNota.conten"></b-textarea>
-          </b-modal>
-        
+      </b-card>
 
-        <b-modal
-        id="error-msg"
-        title="error"
-        >  
-            <h1>error al enviar la nota</h1>
-        </b-modal>
-        <b-modal
-        id="error-empty"
-        title="error"
-        >  
-        <h4>error los campos estan vacios! escribe tanto nombre como cuerpo
-        </h4>
-        </b-modal>
+      <b-button
+        class="btn-sm ml-2"
+        @click="$bvModal.hide('notas-modal')"
+        variant="secondary"
+        >cerrar</b-button
+      >
+      <b-button variant="success" class="ml-2 btn-sm" v-b-modal.add-note
+        >agregar</b-button
+      >
+      <b-modal id="add-note" title="extra" @ok="sendNote">
+        <b-badge variant="dark">nombre</b-badge>
+        <b-input class="my-2" v-model="nuevaNota.nombre"></b-input>
+        <b-badge variant="dark">contenido</b-badge>
+        <b-textarea class="my-2" v-model="nuevaNota.conten"></b-textarea>
+      </b-modal>
 
-        <b-modal id="notas-show" title="contenido de la nota">
-            <b-badge variant="dark">{{outputNote.nombre}}</b-badge> <br>
-            <span class="font-weight-bolder text-secondary rounded">  {{outputNote.conten}} </span>
-        </b-modal>
+      <b-modal id="error-msg" title="error">
+        <h1>error al enviar la nota</h1>
+      </b-modal>
+      <b-modal id="error-empty" title="error">
+        <h4>error los campos estan vacios! escribe tanto nombre como cuerpo</h4>
+      </b-modal>
 
-     </b-card>
-    </b-container>
-
-
+      <b-modal id="notas-show" title="contenido de la nota">
+        <b-badge variant="dark">{{ outputNote.nombre }}</b-badge> <br />
+        <span class="font-weight-bolder text-secondary rounded">
+          {{ outputNote.conten }}
+        </span>
+      </b-modal>
+    </b-card>
+  </b-container>
 </template>
 
-
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    name: 'notas',
-    data() {
-        return {
-            api: this.GLOBAL.API,
-            init: false,
-            nombre: '',
-            identity: localStorage.getItem('inited'),
-            seed: Math.random()*100 + new Date().getSeconds(),
-            notes: [
+  name: "notas",
 
-            ],
-            field: ['nombre'],
-            nuevaNota: {
-                nombre: '',
-                conten: '',
-                author: this.identity
-            },
-            outputNote: {
-                 nombre: '',
-                conten: ''
-            }
-        }
+  methods: {
+    saveme() {
+      this.$store.dispatch("saveme", { vm: this });
     },
-    methods: {
-        saveme() {
-            if(this.nombre != '') {
-            const notesSeed = this.nombre+this.seed;
-            localStorage.setItem('inited',notesSeed); 
-            this.$bvModal.hide('notas-modal');
-            this.chargeNotes();
-            }
-        },
-      async chargeNotes() {
-           const response = await this.axios.get(`${this.api}notes/recollector`);
-           this.notes = response.data;
-        },
-     async sendNote() {
-          
-          if(this.nuevaNota.nombre != '' && this.nuevaNota.conten != '') 
-          {   
-           try {
-               await this.axios({
-               method: 'POST',
-               url: `${this.api}notes/new`,
-               data: this.nuevaNota
-           });
-           this.nuevaNota.nombre = '';
-           this.nuevaNota.conten = '';
-             this.$bvModal.hide('notas-modal');
-             this.$bvModal.show('notas-modal');
-           } catch {
-               this.$bvModal.show('error-msg');
-           }
-          } else {
-               this.$bvModal.show('error-empty');
-          }
+    chargeNotes() {
+      this.$store.commit("chargeNotes");
+    },
+    sendNote() {
+      this.$store.commit("sendNote", this);
+    },
+    showNote(id) {
+      this.$store.commit("showNote", { id: id, vm: this });
+    },
+    deleteNote(id) {
+      this.$store.commit("deleteNote", { id: id, vm: this });
+    },
+  },
+  computed: {
+    ...mapGetters({
+      field: "field",
+      seed: "noteSeed",
+    }),
 
-        },
+    nuevaNota: {
+      get() {
+        return this.$store.state.nuevaNota;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", {
+          type: "nuevaNota",
+          data: value,
+        });
+      },
+    },
 
-       async showNote(id) {
-            const nota = await this.axios.get(`${this.api}notes/show?id=${id}`);
-            this.outputNote.nombre =nota.data.nombre;
-            this.outputNote.conten = nota.data.conten;
-            this.$bvModal.show('notas-show');
-        },
-      async deleteNote(id) {
-         await this.axios.delete(`${this.api}notes/delete?id=${id}`);
-          this.$bvModal.hide('notas-modal');
-          this.$bvModal.show('notas-modal');
-      }
+    outputNote: {
+      get() {
+        return this.$store.state.outputNote;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", {
+          type: "outputNote",
+          data: value,
+        });
+      },
     },
-    computed: {
-        filterData() {
-            return this.notes.filter( item => item.author.includes(this.identity));
-        }
+
+    identity: {
+      get() {
+        return this.$store.state.identity;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", {
+          type: "identity",
+          data: value,
+        });
+      },
     },
-    mounted() {
-       this.init = localStorage.getItem('inited') ? true : false;
-       if(this.init) {
-           this.identity = localStorage.getItem('inited');
-           this.nuevaNota.author = localStorage.getItem('inited');
-           this.chargeNotes();
-       }
+    notes: {
+      get() {
+        return this.$store.state.notes;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", { type: "notes", data: value });
+      },
+    },
+    init: {
+      get() {
+        return this.$store.state.init;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", { type: "init", data: value });
+      },
+    },
+    nombre: {
+      get() {
+        return this.$store.state.nombre;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", { type: "nombre", data: value });
+      },
+    },
+    api: {
+      get() {
+        return this.$store.state.API;
+      },
+      set(value) {
+        this.$store.commit("superNoteUpdate", { type: "api", data: value });
+      },
+    },
+
+    filterData() {
+      return this.$store.state.notes.filter((item) =>
+        item.author.includes(this.identity)
+      );
+    },
+  },
+  mounted() {
+    this.init = localStorage.getItem("inited") ? true : false;
+    if (this.init) {
+      this.identity = localStorage.getItem("inited");
+      this.$store.state.nuevaNota.author = localStorage.getItem("inited");
+      this.chargeNotes();
     }
-}
+  },
+};
 </script>
--- src/main.js
+++ src/main.js
@@ -1,27 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import VueAxios from 'vue-axios'
+import store from './store'
+
 import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-
-import global from './components/global.vue'
 import {BootstrapVue, IconsPlugin, BootstrapVueIcons} from 'bootstrap-vue'
 
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'es6-promise/auto'
 
 import vue_itextarea from 'vue-itextarea';
 
 Vue.config.productionTip = false
-Vue.prototype.GLOBAL = global;
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 Vue.use(BootstrapVueIcons)
-Vue.use(VueAxios, axios)
 Vue.component('vue-itextarea', vue_itextarea);
 
+Vue.use(VueAxios, axios);
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
