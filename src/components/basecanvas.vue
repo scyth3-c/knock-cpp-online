7cc8cf2214fde783d5866d5fc26aee90c3abbb65
scyth3-c <kevintor2246@gmail.com>
2022-01-08 23:35:04 -0500

    highlights

--- src/components/basecanvas.vue
+++ src/components/basecanvas.vue
@@ -1,13 +1,14 @@
 <template>
   <b-container>
     <b-card
-      class="col-md-12 mb-3 bg-dark w-100 mt-2 shadow-lg"
-      style="min-height: 530px;"
+      :class="bytheme"
+      class="dracula col-md-12 mb-3 w-100 mt-2 shadow-lg"
     >
       <b-container style="display:inline;">
       <b-form-select
-        class="text-center mt-1 bg-dark text-white col-sm-2 font-weight-bold"
+        class="text-center mt-1 text-info col-sm-2 font-weight-bold"
         v-model="standar"
+        style="background: rgba(0,0,0,0);"
         title="el estandar con el que se compilara c++"
       >
         <b-form-select-option value="c++11">c++11</b-form-select-option>
@@ -16,41 +17,49 @@
         <b-form-select-option value="c++2a">c++2a</b-form-select-option>
       </b-form-select>
         <b-form-select
-        class="ml-3 mt-1 text-center bg-dark text-white col-md-1 font-weight-bold"
+        class="ml-3 mt-1 text-center text-info col-md-1 font-weight-bold"
         v-model="optimizar"
+        style="background: rgba(0,0,0,0);"
         title="el nivel de optimizacion a la hora de compilar, esto se vera en el codigo ensamblador"
         >
         <b-form-select-option value="1">O1</b-form-select-option>
         <b-form-select-option value="2">O2</b-form-select-option>
         <b-form-select-option value="3">O3</b-form-select-option>
       </b-form-select>
-      <b-button title="descarga el codigo assembly generado del codigo c++" @click="getAssembly" variant="dark" class="mt-1 ml-3 btn btn-outline-info">ASM</b-button>
-      <b-button title="descarga el codigo escrito" @click="download" variant="dark" class="mt-1 ml-2"
+      <b-button title="descarga el codigo assembly generado del codigo c++" @click="getAssembly" style="background: rgba(0,0,0,0);" class=" mt-1 ml-3">ASM</b-button>
+      <b-button title="descarga el codigo escrito" @click="download" style="background: rgba(0,0,0,0);" class="mt-1 ml-2"
         ><b-icon icon="download" variant="white"></b-icon
       ></b-button>  
-          <b-button variant="dark" class="float-right ml-auto" v-b-modal.extra-modal title="muestra el panel de addons extra"> <b-icon variant="white" icon="easel-fill"></b-icon> </b-button>
+
+
+          <b-button style="background: rgba(0,0,0,0);" class="float-right ml-auto" v-b-modal.extra-modal title="muestra el panel de addons extra"> <b-icon variant="white" icon="easel-fill"></b-icon> </b-button>
           <b-modal id="extra-modal" title="extra">
+
             <b-button v-b-modal.notas-modal variant="primary">Notas</b-button>
             <b-modal id="notas-modal" hide-footer>
               <notas/>
             </b-modal>
+
+            <b-button class="ml-1" v-b-modal.themes-modal variant="primary">themes</b-button>
+            <b-modal id="themes-modal">
+              <themes/>
+            </b-modal>
+
+
           </b-modal>
+
+
+
       </b-container>
       <div class="row">
-        <div class="contencore rounded ml-1 col-md-12 shadow-md mt-2">
-          <vue-itextarea
-            v-model="code"
-            autofocus
-            required
-            class="codecore mt-2 font-weight-bold"
-            name="code"
-            id="content"
-            rows="13"
-          ></vue-itextarea>
+        <div class="rounded col-md-12 shadow-md mt-2">
+
+          <codemirror v-model="code" :options="cmOption" />
+
         </div>
         <b-card
-          class="w-100 ml-2 bg-dark mt-2"
-          style="max-height: 120px; min-height: 120px; overflow-y:auto; border:none;"
+          class="w-100 mt-2"
+          style="background:rgba(0,0,0,0); max-height: 120px; min-height: 120px; overflow-y:auto; border:none;"
         >
           <b-badge variant="danger" class="text-white"
             >output:
@@ -70,17 +79,35 @@
 <script>
 
 import notas from './extra/notas.vue'
+import themes from './extra/themes.vue'
 import {mapGetters} from 'vuex'
+import { codemirror } from 'vue-codemirror'
+import 'codemirror/lib/codemirror.css'
 
-export default {
+import 'codemirror/theme/dracula.css'
+import 'codemirror/theme/yonce.css'
+import 'codemirror/theme/monokai.css'
+import 'codemirror/theme/base16-dark.css'
+import 'codemirror/theme/base16-light.css'
+
+
+import 'codemirror/mode/clike/clike.js'
+import 'codemirror/keymap/sublime.js'
+
+export default {  
 
   name: "basecanvas",
   components: {
-    notas
+    notas,
+    themes,
+    codemirror
   },
 
   created() {
     this.charge();
+    this.$root.$on('Theme',()=>{
+      this.$forceUpdate();
+    });
   },
 
 
@@ -118,7 +145,8 @@ export default {
 
      ...mapGetters([
         'time',
-        'seed'
+        'seed',
+        'cmOption'
      ]),
       
       standar: {
@@ -146,11 +174,13 @@ export default {
       optimizar: {
         get() {        return this.$store.state.optimizar;     },
         set(value) {   this.$store.commit('superUpdate',  {type: 'optimizar', data: value});  }
-      }
+      },
 
+      bytheme() {
+          return this.$store.state.bytheme;
+      }
 }   
   
-
 
 };
 </script>
@@ -160,32 +190,23 @@ export default {
 
 
 
-<style>
-.codecore {
-  background-color: rgb(13, 33, 51);
-  color: rgb(139, 223, 230);
-  width: 99%;
-  border: none;
-  outline: 0;
-  font-family: monospace;
+<style scoped>
+
+.dracula {
+  background: rgb(40, 42, 54);
+  min-height: 530px;
 }
-.contencore {
-  background-color: rgb(13, 33, 51);
-  min-height: 300px;
-  border-bottom: 10px solid rgb(10, 22, 32);
+.monokai {
+  background: rgb(39, 40, 34);
 }
-::-webkit-scrollbar {
-  width: 10px;
-  border: none;
+.yonce {
+  background: rgb(28, 28, 28);
 }
-::-webkit-scrollbar-track {
-  background: rgb(13, 33, 51);
+.base16-dark { 
+  background: rgb(21, 21, 21);
 }
-::-webkit-scrollbar-thumb {
-  background: rgb(19, 41, 61);
-  border-radius: 5px;
+.default {
+  background: rgb(199, 200, 204);
 }
-::-webkit-scrollbar-thumb:hover {
-  background: rgb(13, 33, 51);
-}
+
 </style>
---
+++ src/components/extra/themes.vue
@@ -0,0 +1,34 @@
+<template>
+    
+    <b-card>
+
+         <b-form-select
+        v-model="cmOption"
+        title="el tema del editor"
+      >
+        <b-form-select-option value="dracula">dracula</b-form-select-option>
+        <b-form-select-option value="monokai">monokai</b-form-select-option>
+        <b-form-select-option value="yonce">yonce</b-form-select-option>
+        <b-form-select-option value="base16-dark">dark</b-form-select-option>
+         <b-form-select-option value="default">light</b-form-select-option>
+      </b-form-select>
+
+
+    </b-card>
+
+</template>
+
+<script>
+export default {
+    name: 'themes',
+    computed: {
+        cmOption: {
+            get() {
+                return this.$store.state.cmOption.theme;
+            }, set(value) {
+                this.$store.commit('changeTheme',{data: value, vm: this});
+            }
+        }
+    }
+}
+</script>
