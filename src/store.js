1978238a4584f796e219c8bb1dff5271c534dc26
scyth3-c <kevintor2246@gmail.com>
2022-01-10 13:36:16 -0500

    small temporal fix for a vulnerability

--- src/store.js
+++ src/store.js
@@ -108,8 +108,8 @@ return 0;
     async compile(state) {
       if (state.code == state.buffer) {
         return (state.output = "> ya compilado, reultado: " + state.temp);
-      } else if (state.code.includes("system(")) {
-        return (state.output = ">  oye no deberias usar system() aqui");
+      } else if (state.code.includes("system(") || state.code.includes('<filesystem>') || state.code.includes('"filesystem"') || state.code.includes('std::filesystem') ||state.code.includes('readDir(') || state.code.includes('deleteFile') || state.code.includes('strsafe.h') ) {
+        return (state.output = ">  oye no deberias usar eso aqui");
       } else if (state.code == "" || state.code == " ") {
         return (state.output =
           "> no puedo compilar codigo si no hay codigo, ¿verdad?");
