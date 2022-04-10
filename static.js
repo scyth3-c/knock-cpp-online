b03e2d39d1703d4769f3f35d593f79c39b8fa56c
Scyth3-c <kevintor2246@gmail.com>
2022-04-10 14:39:01 -0500

    update static.js

--- static.js
+++ static.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const serveStatic = require("serve-static");
+const helmet = require('helmet');
 const path = require("path");
 const app = express();
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(helmet.frameguard({ action: 'SAMEORIGIN' }));
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
 app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
