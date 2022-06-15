40f913e90b0d7004ea3fc5a74586e90340be2235
Scyth3-c <kevintor2246@gmail.com>
2022-06-14 19:54:24 -0500

    create headers.vue and tabbar.vue, this is necesary for, headers build total code to compile, and tabbar for manage tabs

---
+++ src/components/extra/headers.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-badge class="p-2" variant="white">set the headers here: </b-badge>
+    <b-input
+      class="text-black font-weight-bold"
+      v-model="headers"
+      placeholder="iostream math vector"
+    ></b-input>
+  </div>
+</template>
+
+<script>
+import store from "@/store";
+export default {
+  name: "headers",
+  computed: {
+    headers: {
+      get() {
+        return store.state.headers;
+      },
+      set(value) {
+        store.state.headers = value;
+      },
+    },
+  },
+};
+</script>
---
+++ src/components/extra/tabbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :id="id" @click="changeTab" class="ml-2 tabs">
+    <b-icon icon="file-code"></b-icon>
+    <b-icon @click="delspace()" scale="1" variant="white" icon="x"></b-icon>
+  </button>
+</template>
+
+<script>
+import store from "@/store";
+export default {
+  name: "tabbar",
+  props: ["id"],
+  methods: {
+    changeTab() {
+      store.commit("setSpace", { id: this.id, vm: this });
+    },
+    delspace() {
+      store.commit("deleteSpace", { id: this.id });
+    },
+  },
+};
+</script>
+
+<style>
+.tabs {
+  width: 10%;
+  height: 28px;
+  border-radius: 4px;
+  background: transparent;
+  border: 1px solid white;
+  color: white;
+}
+</style>
