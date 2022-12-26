c6dcbb592f8af93af17b304357644fe256fdfe7f
Kevin Bohorquez <kevintor2246@gmail.com>
2022-12-25 21:38:49 -0500

    re-desing and update responsive

--- src/components/extra/tabbar.vue
+++ src/components/extra/tabbar.vue
@@ -1,7 +1,7 @@
 <template>
   <button :id="id" @click="changeTab" class="ml-2 tabs">
     <b-icon title="agrega una tab" icon="file-code"></b-icon>
-    <b-icon title="cierra la tab" @click="delspace()" scale="1" variant="white" icon="x"></b-icon>
+    <b-icon title="cierra la tab" @click="delspace()" scale="1" icon="x"></b-icon>
   </button>
 </template>
 
@@ -27,7 +27,15 @@ export default {
   height: 28px;
   border-radius: 4px;
   background: transparent;
-  border: 1px solid white;
+  border: 1px solid #A5D6A7 ;
   color: white;
+  margin-bottom:10px;
 }
+
+.tabs:hover{
+  background-color:#A5D6A7;
+  transition: 0.5s;
+  color: black;
+}
+
 </style>
